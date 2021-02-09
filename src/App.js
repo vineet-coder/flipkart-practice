d962ba16fc09dc08e8a002a0e854c8106a528475
vineet-coder <77148160+vineet-coder@users.noreply.github.com>
2021-02-10 00:24:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+<nav >
+    <div className="baner">   <label className="brandName">flipkart</label>
+       <input className="search"></input>
+       <button className="banerButton">Search</button>
+       <div></div>
+       </div>
+     </nav>
+    </div>
+  );
+}
