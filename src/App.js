f362e1444bba5096aedf2411a58fb6bc0d25d3da
vineet-coder <77148160+vineet-coder@users.noreply.github.com>
2021-02-10 00:30:30 +0530

    add

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-<nav >
-    <div className="baner">   <label className="brandName">flipkart</label>
-       <input className="search"></input>
-       <button className="banerButton">Search</button>
-       <div></div>
-       </div>
-     </nav>
+      <nav>
+        <div className="baner">
+          <label className="brandName">flipkart</label>
+          <input className="search"></input>
+          <button className="banerButton">Search</button>
+          <div>more</div>
+        </div>
+      </nav>
     </div>
   );
 }
